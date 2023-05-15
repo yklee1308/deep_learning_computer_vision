ea853be9d0dd1bac1e08c5635a8a7e76d0cc7c36
yklee1308 <yklee1308@gmail.com>
2023-05-16 00:32:53 +0900

    detection : add selective search R-CNN processing

--- detection/processings/rcnn_processing.py
+++ detection/processings/rcnn_processing.py
@@ -1,4 +1,6 @@
+import numpy as np
 from matplotlib import pyplot as plt
+from selectivesearch import selective_search
 
 import torch
 
@@ -7,6 +9,14 @@ class RCNNProcessing(object):
     def __init__(self, dataset):
         self.classes = dataset.classes
         self.channels = dataset.img_shape[0]
+
+        # Selective Search
+        self.num_regions = 128
+
+    def preprocess(self, x, y):
+        regions = self.runSelectiveSearch(x[0], num_regions=self.num_regions)
+
+        return x, y
 
     def visualize(self, x, y, img_path):
         x, y, img_path = torch.argmax(x).item(), y[0].item(), img_path[0]
@@ -20,3 +30,8 @@ class RCNNProcessing(object):
         plt.title('[Prediction] {} [Label] {}'.format(self.classes[x], self.classes[y]))
 
         plt.show()
+
+    def runSelectiveSearch(self, img, num_regions):
+        _, regions = selective_search(np.array(img, dtype=np.uint8), scale=500, sigma=0.9, min_size=10)
+
+        return regions
