7374120bfeca73969f3cd9687b952f21157d750e
yklee1308 <yklee1308@gmail.com>
2023-05-28 23:57:02 +0900

    detection : apply IoU

--- detection/processings/rcnn_processing.py
+++ detection/processings/rcnn_processing.py
@@ -12,22 +12,29 @@ class RCNNProcessing(object):
         self.classes = dataset.classes
         self.transform = dataset.transform
 
-        # Selective Search
         self.num_regions = 128
+        self.positive_ratio = 0.25
+        self.iou_th = 0.5
 
     def preprocess(self, x, y):
         img, label = np.array(x[0], dtype=np.uint8), y[0]
 
-        regions = self.runSelectiveSearch(img, num_regions=self.num_regions)
+        regions = self.runSelectiveSearch(img)
 
         x, y = list(), list(list() for i in range(2))
-        for i in range(self.num_regions):
-            tl_x, tl_y, br_x, br_y = regions[i]
-            sample = img[tl_y:br_y , tl_x:br_x]
-            sample = transforms.ToPILImage()(sample)
-            sample = transforms.Resize(size=self.img_shape[1:])(sample)
-            sample = self.transform(sample)
-            x.append(sample)
+        for region in regions:
+            ious = list()
+            for bbox in label[1]:
+                iou = self.computeIoU(region, bbox)
+                ious.append(iou)
+
+            if max(ious) > self.iou_th:
+                tl_x, tl_y, br_x, br_y = region
+                sample = img[tl_y:br_y , tl_x:br_x]
+                sample = transforms.ToPILImage()(sample)
+                sample = transforms.Resize(size=self.img_shape[1:])(sample)
+                sample = self.transform(sample)
+                x.append(sample)
 
         x, y = torch.stack(x, dim=0), list(torch.stack(target, dim=0) for target in y)
 
@@ -46,8 +53,8 @@ class RCNNProcessing(object):
 
         plt.show()
 
-    def runSelectiveSearch(self, img, num_regions):
-        _, region_data = selective_search(img, scale=100, sigma=0.9, min_size=100)
+    def runSelectiveSearch(self, img):
+        _, region_data = selective_search(img, scale=100, sigma=0.8, min_size=100)
         region_data = sorted(region_data, key=lambda x: x['size'], reverse=True)
 
         regions = list()
@@ -56,8 +63,6 @@ class RCNNProcessing(object):
             region = (x, y, x + w + 1, y + h + 1)
             if region not in regions:
                 regions.append(region)
-
-        regions = regions[:num_regions]
 
         return regions
     
