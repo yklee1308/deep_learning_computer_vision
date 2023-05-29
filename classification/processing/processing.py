77300cbfba9990bc6f2079ee9513a93ac450718e
yklee1308 <yklee1308@gmail.com>
2023-05-30 01:06:23 +0900

    classification : minor variable change

--- classification/processing/processing.py
+++ classification/processing/processing.py
@@ -5,16 +5,16 @@ import torch
 
 class Processing(object):
     def __init__(self, dataset):
+        self.img_shape = dataset.img_shape
         self.classes = dataset.classes
-        self.channels = dataset.img_shape[0]
 
     def visualize(self, x, y, img_path):
         x, y, img_path = torch.argmax(x).item(), y[0].item(), img_path[0]
 
         img = plt.imread(fname=img_path)
-        if self.channels == 1:
+        if self.img_shape[0] == 1:
             plt.imshow(img, cmap='gray')
-        elif self.channels == 3:
+        elif self.img_shape[0] == 3:
             plt.imshow(img)
             
         plt.title('[Prediction] {} [Label] {}'.format(self.classes[x], self.classes[y]))
