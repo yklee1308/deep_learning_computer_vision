5e7ab42568ccc18409a8618ad1960a3306242ae2
yklee1308 <yklee1308@gmail.com>
2023-05-29 22:36:02 +0900

    classification : minor indentation change

--- classification/configs.py
+++ classification/configs.py
@@ -16,7 +16,6 @@ parser.add_argument('--img_shape', type=tuple, default=(1, 32, 32), help='img_sh
 parser.add_argument('--loss_function', type=str, default='CE', help='loss_function')
 
 parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer')
-
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=0.0005, help='weight decay')
 parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate')
